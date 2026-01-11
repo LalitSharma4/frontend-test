2dd58d2389a6fe6229ea59d818464d21b920d419
LalitSharma4 <iamlalitsharma4@gmail.com>
2026-01-11 16:30:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-indigo-600">
+        Clinic project ready
+      </h1> 
+    </>
+  )
+}
+
+export default App
