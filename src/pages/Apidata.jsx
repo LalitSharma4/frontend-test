19fa56e72bf1b81ff08f94d7fda9f5e126b046c6
LalitSharma4 <iamlalitsharma4@gmail.com>
2026-01-11 17:35:07 +0530

    connect backend api

--- src/pages/Apidata.jsx
+++ src/pages/Apidata.jsx
@@ -1,10 +1,30 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import api from "../services/api";
 
 export default function ApiData() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    api.get("products/")
+      .then((res) => {
+        console.log(res.data); // see response
+        setProducts(res.data.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>API Data Page</h1>
-      <Link to="/">Go Home</Link>
+      <h1>Products</h1>
+
+      {products.map((p) => (
+        <div key={p.id}>
+          <p>{p.name}</p>
+          <p>₹ {p.price}</p>
+        </div>
+      ))}
     </div>
   );
 }
