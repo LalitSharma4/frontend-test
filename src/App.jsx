35909b938a098f06f357489791ff114ac270e081
LalitSharma4 <iamlalitsharma4@gmail.com>
2026-01-11 16:34:58 +0530

    add more commits

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-4xl font-bold text-indigo-600">
-        Clinic project ready
+        Clinic project ready on vercel
       </h1> 
     </>
   )
